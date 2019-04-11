b38688091ed7bbf6f632b9315faa3aebf29571cf
Johann LALLEMENT <jlallement@makheia.com>
2019-04-11 16:51:26 +0200

    initial commit

---
+++ src/Entity/Product.php
@@ -0,0 +1,64 @@
+<?php
+
+
+namespace App\Entity;
+
+use ApiPlatform\Core\Annotation\ApiResource;
+use Doctrine\ORM\Mapping as ORM;
+use Symfony\Component\Validator\Constraints as Assert;
+
+/**
+ * This is a dummy entity. Remove it!
+ *
+ * @ApiResource
+ * @ORM\Entity
+ */
+class Product
+{
+    /**
+     * @var int The entity Id
+     *
+     * @ORM\Id
+     * @ORM\GeneratedValue
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @var string A nice person
+     *
+     * @ORM\Column
+     * @Assert\NotBlank
+     */
+    public $name = '';
+
+    /**
+     * @var integer A nice price
+     *
+     * @ORM\Column
+     * @Assert\NotBlank
+     */
+    public $price = '';
+
+    public function getId(): int
+    {
+        return $this->id;
+    }
+
+    /**
+     * @return int
+     */
+    public function getPrice(): int
+    {
+        return $this->price;
+    }
+
+    /**
+     * @param int $price
+     */
+    public function setPrice(int $price): void
+    {
+        $this->price = $price;
+    }
+
+}
